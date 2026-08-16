d0ba8d0d6ee391f1f48cad5b9ed73fccef74516a
Kioffrx <entiquezometa@gmail.com>
2026-08-15 18:14:44 -0600

    feat: agregar layout del ejercicio de salario

--- app/src/main/java/com/example/desafiodsm/MainActivity.kt
+++ app/src/main/java/com/example/desafiodsm/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.desafiodsm
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnEjercicio1 = findViewById<Button>(R.id.btnEjercicio1)
+        val btnEjercicio2 = findViewById<Button>(R.id.btnEjercicio2)
+        val btnEjercicio3 = findViewById<Button>(R.id.btnEjercicio3)
+
+        btnEjercicio1.setOnClickListener {
+            val intent = Intent(this, PromedioActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnEjercicio2.setOnClickListener {
+            Toast.makeText(this, "Ejercicio 2 aún no implementado", Toast.LENGTH_SHORT).show()
+        }
+
+        btnEjercicio3.setOnClickListener {
+            Toast.makeText(this, "Ejercicio 3 aún no implementado", Toast.LENGTH_SHORT).show()
         }
     }
 }
